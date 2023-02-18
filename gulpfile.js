a050d40c8503323ee015bdabcbf47b5c704488cb
Erick Rick <erickcredjes@gmail.com>
2023-02-18 15:10:03 -0300

    Sessão de Planos
    Apresentacão dos valores de planos

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const sourcemap = require("gulp-sourcemaps");
 const imagemin = require("gulp-imagemin");
 const html = require("gulp-html");
-const gulpReplace = require("gulp-replace");
+const uglify = require("gulp-uglify");
 
 function styles() {
     return gulp.src("./src/styles/*.scss")
+        .pipe(sourcemap.init())
         .pipe( sass({ outputStyle : "compressed" }) )
         .pipe(gulp.dest("./dist/css"));
 }
@@ -22,14 +24,15 @@ function htmlPro() {
         .pipe(gulp.dest("./dist/"));
 }
 
-function htmlReplace() {
-    return gulp.src(["./src/*.html"])
-        .pipe(gulpReplace('../dist/css/main.css', './dist/css/main.css'))
-        .pipe(gulp.dest("./dist/"))
+function scriptJs() {
+    return gulp.src("./src/scripts/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/scripts/"));
 }
 
 exports.default = gulp.parallel([styles, minificaImagem, htmlPro]);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
     gulp.watch("./src/*.html", gulp.parallel(htmlPro));
+    gulp.watch("./src/scripts/*.js", gulp.parallel(scriptJs))
 }
