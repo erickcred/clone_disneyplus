fc925aaaedcab94e3841b267b5d96bec4c289f26
Erick Rick <erickcredjes@gmail.com>
2023-02-18 08:47:59 -0300

    Hero e Fonts
    Criando o Hero e importando as font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const html = require("gulp-html");
+const gulpReplace = require("gulp-replace");
 
 function styles() {
     return gulp.src("./src/styles/*.scss")
@@ -7,7 +10,26 @@ function styles() {
         .pipe(gulp.dest("./dist/css"));
 }
 
-exports.default = styles;
+function minificaImagem() {
+    return gulp.src("./src/images/**/*")
+        .pipe( imagemin())
+        .pipe(gulp.dest("./dist/images/"))
+}
+
+function htmlPro() {
+    return gulp.src("./src/*.html")
+        .pipe(html())
+        .pipe(gulp.dest("./dist/"));
+}
+
+function htmlReplace() {
+    return gulp.src(["./src/*.html"])
+        .pipe(gulpReplace('../dist/css/main.css', './dist/css/main.css'))
+        .pipe(gulp.dest("./dist/"))
+}
+
+exports.default = gulp.parallel([styles, minificaImagem, htmlPro]);
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+    gulp.watch("./src/*.html", gulp.parallel(htmlPro));
 }
